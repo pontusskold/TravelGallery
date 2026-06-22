81417a983328fb8eb087682178eb309af0cb1836
Pontus <s300377@oslomet.no>
2026-06-22 19:54:39 +0200

    Add trip details route

--- frontend/travel-gallery/src/app/app.routes.ts
+++ frontend/travel-gallery/src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 
+import { TripDetailsPage } from './features/trips/pages/trip-details-page/trip-details-page';
 import { TripsPage } from './features/trips/pages/trips-page/trips-page';
 
 export const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: 'trips',
+  },
+  {
+    path: 'trips',
     component: TripsPage,
+  },
+  {
+    path: 'trips/:id',
+    component: TripDetailsPage,
   },
 ];
